49be645d1ee63ecf239296d8c67d8c31b079adec
Jared <jared.prince133@topper.wku.edu>
2018-05-22 21:17:05 -0500

    Function node added. Last commit before attempting to integrate new nodes and remove old nodes (Binary, Ternary, Quaternary).

--- src/parser/FunctionNode.java
+++ src/parser/FunctionNode.java
@@ -1,5 +1,36 @@
 package parser;
 
-public class FunctionNode {
+import java.util.ArrayList;
 
+public class FunctionNode extends ASTNode {
+	ASTNode name;
+	ArrayList<DeclarationNode> params; //change to param list node
+	StructureBodyNode body;
+
+	public FunctionNode(ASTNode name, ArrayList<DeclarationNode> l, Token t, StructureBodyNode r) {
+		this.name = name;
+		params = l;
+		token = t;
+		body = r;
+	}
+
+	public String toString(int depth) {
+		String str = "";
+		for (int i = 0; i < depth; i++) {
+			str = str + "  ";
+		}
+		return str + "Token: " + token.value + "\n" + "Parameters" + "\n" + body.toString(depth + 1);
+	}
+
+	public TypedObject visitNode() {
+		
+//		//TODO: update 'Function' to match new params
+//		if(Parser.environment.depth > 0){
+//			Parser.environment.define(token, name.token, new TypedObject("func", new Function(params, body, Parser.environment.getScopeFromInner(0))));
+//		}
+//		else {
+//			Parser.environment.define(token, name.token, new TypedObject("func", new Function(params, body)));
+//		}
+		return null;
+	}
 }
